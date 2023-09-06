5fe893720260080d09bf60b6f8e2152b69730de3
Matthieu <lozanomatthieu37@gmail.com>
2023-09-06 08:56:47 +0200

    add: first list part to Readme

--- README.md
+++ README.md
@@ -11,3 +11,49 @@ python3 -m venv .venv
 source .venv/bin/activate
 ```
 
+3. Run .py file 
+```
+python file_name.py
+```
+--- 
+
+## Les listes 
+
+- Declare a list 
+```
+list = [0, 1.5, 'abcd']
+```
+
+### 4 important methods 
+
+#### range()
+- The range() function returns a sequence of numbers, starting 
+from 0 by default, and increments by 1 (by default), and stops before a specified number.
+- The advantages is that don't create a list, just an iterator so very few memory is used by 
+this method. 
+
+- Syntax :
+``` python
+# Only the first arg is require
+range(start, stop, step)
+```
+
+- Exemple :
+```
+range_ten = range(10)
+for i in range_ten:
+    print(i)
+```
+> output :
+```
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+```
--- liste.py
+++ liste.py
@@ -14,24 +14,47 @@ next(iterator) # => Le second élément de ma liste
 ## 2. Boucles
 
 # For basic
-for item in liste :
+for item in liste:
     print(item)
 
 # Les 4 méthodes principales 
 
-## range() Gros avantage => Ne prend presque pas de mémoire
+
+## 1. range() Gros avantage => Ne prend presque pas de mémoire
 ## range() ne créé les éléments qu'a la damande de next()
 range_dix = range(10) # Donne un itérable de 0 à 10.
 liste_dix = list(range_dix) # Donne une liste [0,1...10]
-range_big = range(100000000) # Nécessite très peu de ressource.
+range_big = range(100000) # Nécessite très peu de ressource.
 liste_big = list(range_big) # Nécessite TROP de ressources 
+range_deux_args = range(10, 100) # 10 ==> 99
+range_trois_args = range(10, 20, 2) # 10, 12, 14 ==> 18
 
 # Utilisation la plus courante =>
-a = 0
-for i in range_big :
+for i in range_dix:
     a = i
 
-print(a) # => Renvoi 999999999 sans utiliser de rame
+
+## 2. enumerate() récupére la valeur ET l'index
+
+for idx, obj in enumerate(["a", "b", "c"]):
+    print(idx, obj)
+# Retourne => (0, 'a') - (1, 'b') - (2, 'c')
 
 
+## 3. reversed() inverse l'ordre d'une collection en retournant un itérateur
 
+for obj in reversed(['c', 'b', 'a']):
+    print(obj)
+# Retourne => 'a', 'b', 'c'
+
+
+## 4. zip() permet d'unir deux listes 
+plat = ['blanquette de veaux', 'poulet roti']
+prix = [12, 23, 34]
+devise = ['euros', 'yens', 'roubles']
+
+for obj in zip(plat, prix, devise):
+    print(obj)
+# Retourne => ('blanquette de veaux', 12, 'euros') - ('poulet roti', 23) - ('risotto', 7)
+for plat, prix, devise in zip(plat, prix, devise):
+    print('Le ' + plat + ' est à : ' + str(prix) + ' ' + devise)
